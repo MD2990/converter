28483e13e4d56cecf0be0a004e02f553860b5d9b
MD2990 <31358358+MD2990@users.noreply.github.com>
2021-11-18 07:35:16 +0400

    testing

--- components/Main.js
+++ components/Main.js
@@ -16,44 +16,143 @@ import { Radio, RadioGroup } from "@chakra-ui/react";
 export default function Main() {
   const [value, setValue] = useState("1");
   const [value2, setValue2] = useState("1");
-  const [calc, setCalc] = useState('1');
+  const [input, setInput] = useState("1");
   const [results, setResults] = useState(0);
 
+  const getIndex = (a, b) => {
+    //create an array of all units
+    const units = [
+      "meters",
+      "feet",
+      "inches",
+      "centimeters",
+      "kilometers",
+      "miles",
+      "yards",
+      "nanometers",
+      "micrometers",
+      "millimeters",
+    ];
+    const units2 = [
+      "meters",
+      "feet",
+      "inches",
+      "centimeters",
+      "kilometers",
+      "miles",
+      "yards",
+      "nanometers",
+      "micrometers",
+      "millimeters",
+    ];
+    if (a == 1 && b == 1)
+      // if both are the same return nothing
+      setResults(111);
+    if (a == 1 && b == 2) {
+      setResults(value / 1000);
+    }
+
+    // create a new function that converts a meter to kilometer
+
+    // create a new function that converts a kilometer to meter
+    const convertKilometerToMeter = (kilometer) => {
+      return kilometer * 1000;
+    };
+    // create a new function that converts a kilometer to mile
+    const convertKilometerToMile = (kilometer) => {
+      return kilometer / 1.609;
+    };
+    // create a new function that converts a mile to kilometer
+    const convertMileToKilometer = (mile) => {
+      return mile * 1.609;
+    };
+    // create a new function that converts a kilometer to feet
+    const convertKilometerToFeet = (kilometer) => {
+      return kilometer * 3280.84;
+    };
+    // create a new function that converts a feet to kilometer
+    const convertFeetToKilometer = (feet) => {
+      return feet / 3280.84;
+    };
+    // create a new function that converts a kilometer to inch
+    const convertKilometerToInch = (kilometer) => {
+      return kilometer * 39370.1;
+    };
+    // create a new function that converts a inch to kilometer
+    const convertInchToKilometer = (inch) => {
+      return inch / 39370.1;
+    };
+    // create a new function that converts a kilometer to yard
+    const convertKilometerToYard = (kilometer) => {
+      return kilometer * 1093.61;
+    };
+    // create a new function that converts a yard to kilometer
+    const convertYardToKilometer = (yard) => {
+      return yard / 1093.61;
+    };
+    // create a new function that converts a kilometer to centimeter
+    const convertKilometerToCentimeter = (kilometer) => {
+      return kilometer * 100000;
+    };
+    // create a new function that converts a centimeter to kilometer
+    const convertCentimeterToKilometer = (centimeter) => {
+      return centimeter / 100000;
+    };
+    // create a new function that converts a kilometer to millimeter
+    const convertKilometerToMillimeter = (kilometer) => {
+      return kilometer * 1000000;
+    };
+    // create a new function that converts a millimeter to kilometer
+    const convertMillimeterToKilometer = (millimeter) => {
+      return millimeter / 1000000;
+    };
+    // create a new function that converts a kilometer to micrometer
+    const convertKilometerToMicrometer = (kilometer) => {
+      return kilometer * 1000000000;
+    };
+    // create a new function that converts a micrometer to kilometer
+    const convertMicrometerToKilometer = (micrometer) => {
+      return micrometer / 1000000000;
+    };
+    // create a new function that converts a kilometer to nanometer
+    const convertKilometerToNanometer = (kilometer) => {
+      return kilometer * 1000000000000;
+    };
+    // create a new function that converts a nanometer to kilometer
+    const convertNanometerToKilometer = (nanometer) => {
+      return nanometer / 1000000000000;
+    };
+  };
   const handleChange = (e) => {
-
-
+    setValue(e);
+    console.log(getIndex(value, value2, value, results));
     //setCalc(e.target.value);
-    switch (Number(e)) {
-      case 1 :
+    /*   switch (getIndex(Number(e), Number(value2))) {
+      case getIndex(1, 1):
         //setResults(Number(e) * 100);
-        console.log('meter');
+        console.log("meter");
         break;
-      case 2 :
+      case 2:
         //setResults(Number(e) * 100);
-        console.log(results);
+        console.log("results");
         break;
 
       default:
-        setResults(Number(e) * 500);
         break;
-    } 
+    }
 
-     setCalc( e );
+    setCalc(e); */
 
-   // setValue(e);
+    // setValue(e);
   };
   const handleChange2 = (e) => {
     setValue2(e);
-    console.log(e);
   };
 
-
-  const handleInputChange = (e) => setResults( Number(e.target.value)*100);
+  const handleInputChange = (e) => setInput(Number(e.target.value));
 
   return (
     <>
-
- 
       <Wrap justify="center" mt="10%">
         <WrapItem>
           {" "}
@@ -92,15 +191,15 @@ export default function Main() {
             <RadioGroup
               size="sm"
               name="radio"
-              value={calc}
-              onChange={ handleChange}
+              value={value}
+              onChange={handleChange}
             >
               <Stack
                 direction={["column", "row"]}
                 spacing={[1, 2, 4, 5]}
                 divider={<StackDivider borderColor="gray.200" />}
               >
-                <Radio id="Length1" value='1' >
+                <Radio id="Length1" value="1">
                   Meter
                 </Radio>
                 <Radio id="Length2" value="2">
@@ -138,9 +237,9 @@ export default function Main() {
           </WrapItem>
 
           <Input
-            name="calc"
-            placeholder="Search"
-          
+            name="input"
+            value={input}
+            placeholder="Convert"
             onChange={handleInputChange}
           />
           <WrapItem>
@@ -148,7 +247,7 @@ export default function Main() {
               size="sm"
               name="radio"
               value={value2}
-              onChange={(e) => handleChange2(e)} 
+              onChange={(e) => handleChange2(e)}
             >
               <Stack
                 direction={["column", "row"]}
